50149d6056bc05f6a6f2b91224ea04014945b940
klarissamm <klarissamunz@gmail.com>
2017-02-21 19:56:01 +0000

    Added modal to calendar

--- src/js/controllers/periods/index.controller.js
+++ src/js/controllers/periods/index.controller.js
@@ -2,22 +2,53 @@ angular
 .module('avocado')
 .controller('PeriodsIndexCtrl', PeriodsIndexCtrl);
 
-PeriodsIndexCtrl.$inject = ['Period'];
-function PeriodsIndexCtrl(Period){
+PeriodsIndexCtrl.$inject = ['Period', 'calendarConfig', '$uibModal', '$document', '$scope'];
+function PeriodsIndexCtrl(Period, calendarConfig, $uibModal, $document, $scope){
   const vm = this;
   Period
-    .query()
-    .$promise
-    .then(data => {
-      vm.periods = data.map(period => {
-        period.startsAt = new Date(period.date);
-        return period;
-      });
+  .query()
+  .$promise
+  .then(data => {
+    vm.periods = data.map(period => {
+      period.title = 'Show Infomation';
+      period.startsAt = new Date(period.date);
+      period.color = {
+        primary: '#333',
+        secondary: '#e00'
+      };
+      period.incrementsBadgeTotal = false;
+      period.allDay = true;
+      return period;
     });
+  });
+
   vm.calendarView = 'month';
   vm.viewDate     = new Date();
 
-  vm.eventClicked = function(data) {
-    console.log(data);
+  vm.animationsEnabled = true;
+
+  vm.open = function(data, size) {
+
+    var modalInstance = $uibModal.open({
+      animation: vm.animationsEnabled,
+      ariaLabelledBy: 'modal-title',
+      ariaDescribedBy: 'modal-body',
+      templateUrl: '/js/views/periods/modal.html',
+      size: size,
+      appendTo: angular.element($document[0].querySelector('main')),
+      resolve: {
+        data: function () {
+          return data;
+        }
+      }
+    });
+
+    vm.ok = function () {
+      vm.close({$value: vm.selected.item});
+    };
+
+    vm.cancel = function () {
+      vm.dismiss({$value: 'cancel'});
+    };
   };
 }
